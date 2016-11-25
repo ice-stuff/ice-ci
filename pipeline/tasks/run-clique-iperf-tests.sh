22864d73b4636aceb970135e3a242f2bf7d7f38e
George Lestaris <glestaris@gmail.com>
2016-11-25 18:49:32 +0000

    Run clique Iperf tests in CI

---
+++ pipeline/tasks/run-clique-iperf-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+# Move to GOPATH
+mkdir -p $GOPATH/src/github.com/ice-stuff
+cp -r clique $GOPATH/src/github.com/ice-stuff
+cd $GOPATH/src/github.com/ice-stuff/clique/
+
+# Golang dependencies
+go get github.com/Masterminds/glide
+make deps
+go get github.com/onsi/ginkgo/ginkgo
+
+# Build ant test
+make iperf
+make test-iperf
